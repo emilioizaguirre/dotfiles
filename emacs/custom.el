44a2ff258a7e0de4c17b7fac2d245b10e6652428
emilioizaguirre <emilioizaguirre@uchicago.edu>
2025-09-26 12:53:01 -0500

    refactored everything into init file

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,14 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("9e3deb98981367ca3f70fd465a7746a6928dca72357c4b9283abcc7fcacc570a" default))
- '(custom-theme-directory "~/.config/emacs/themes")
- '(icomplete-mode t)
- '(package-selected-packages '(markdown-mode magit evil))
- '(truncate-lines t)
- '(warning-suppress-types '((initialization)))
- '(whitespace-style '(trailing tabs indentation::tab tab-mark)))
+ '(package-selected-packages '(magit multiple-cursors markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/init.el
+++ emacs/init.el
@@ -8,31 +8,49 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-(require 'evil)
-(evil-mode 1)
-(setq completion-auto-help 'nil)
-
-(set-face-attribute 'default nil :family "Ubuntu Sans Mono" :height 200)
-
-(setq inhibit-splash-screen 1)
-(setq backup-directory-alist '(("." . "~/.saves")))
-(setq display-line-numbers-type 'relative)
-(setq visible-bell 1)
-
-(global-display-line-numbers-mode 1)
-(column-number-mode 1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(whitespace-mode 1)
-
+;; package downloads and configures
 (add-to-list 'load-path "~/.config/emacs/modes/simpc-mode/")
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 (setq-default indent-tabs-mode nil)
 
+(unless (package-installed-p 'markdown-mode)
+  (package-install 'markdown-mode))
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+
+(unless (package-installed-p 'multiple-cursors)
+  (package-install 'multiple-cursors))
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+(require 'magit)
+
+;; real config
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(icomplete-mode 1)
+;;(whitespace-mode 1)
+;;(setq whitespace-style '(trailing tabs indentation::tab tab-mark))
+(setq inhibit-splash-screen 1)
+(setq backup-directory-alist '(("." . "~/.saves")))
+(setq display-line-numbers-type 'relative)
+(setq visible-bell t)
+(setq completion-auto-help 'nil)
+(setq-default truncate-lines t)
+
+;; cosmetic stuff
+(setq custom-theme-directory "~/.config/emacs/themes")
+(setq custom-safe-themes '("9e3deb98981367ca3f70fd465a7746a6928dca72357c4b9283abcc7fcacc570a" default))
 (load-theme 'deserted)
 (put 'dired-find-alternate-file 'disabled nil)
+(set-face-attribute 'default nil :family "Ubuntu Sans Mono" :height 200)
+
